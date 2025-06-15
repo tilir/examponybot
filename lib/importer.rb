5bc143adc1006f9df138898eb0042297902c8965
Konstantin Vladimirov <konstantin.vladimirov@gmail.com>
2025-06-15 19:08:40 +0300

    Adding ponymaker

---
+++ lib/importer.rb
@@ -0,0 +1,82 @@
+#------------------------------------------------------------------------------
+#
+# Telegram bot for peering exam on programming
+# Licensed after GNU GPL v3
+#
+#------------------------------------------------------------------------------
+#
+# Exam importer
+#
+#------------------------------------------------------------------------------
+
+require_relative 'handlers'
+require_relative 'pseudoapi'
+
+class QuestionImporter
+  def initialize(filename:, handler:, api:, prepod:, chat:)
+    @filename = filename
+    @handler = handler
+    @api = api
+    @prepod = prepod
+    @chat = chat
+  end
+
+  def import!
+    grouped_questions = parse_file(@filename)
+
+    if grouped_questions.empty? || grouped_questions.all?(&:empty?)
+      raise "Error: no question group found"
+    end
+
+    question_counts = grouped_questions.map(&:size).uniq
+    if question_counts.size > 1
+      raise "Error counts differ: #{question_counts.join(', ')}"
+    end
+
+    grouped_questions.each_with_index do |questions, grp_index|
+      group_id = grp_index + 1
+      puts "Group #{group_id} (#{questions.size} questions)"
+
+      questions.each_with_index do |question_text, question_index|
+        question_id = question_index + 1
+        send_question(group_id, question_id, question_text)
+      end
+    end
+  end
+
+  private
+
+  def parse_file(path)
+    lines = File.read(path).lines.map(&:rstrip)
+
+    groups = []
+    current_group = []
+    in_group = false
+
+    lines.each do |line|
+      if line.strip == '==='
+        groups << current_group unless current_group.empty?
+        current_group = []
+        in_group = true
+      elsif in_group
+        current_group << line
+      end
+    end
+    groups << current_group unless current_group.empty?
+
+    # split group to questions
+    groups.map do |group_lines|
+      group_lines.join("\n").split(/^---$/).map(&:strip).reject(&:empty?)
+    end
+  end
+
+  def send_question(exam_id, question_id, text)
+    command = "/addquestion #{exam_id} #{question_id}"
+    full_text = "#{command}\n#{text}"
+
+    event = PseudoMessage.new(@prepod, @chat, full_text)
+    @handler.process_message(@api, event)
+
+    puts "Q#{exam_id}.#{question_id} added: #{@api.text}"
+  end
+end
---
+++ ponymaker.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+#------------------------------------------------------------------------------
+#
+# Exam maker for ponybot
+# Licensed after GNU GPL v3
+#
+#------------------------------------------------------------------------------
+#
+# Main file, start execution here
+# example:
+# ./ponymaker.rb -f 'your file' -o 'exam.db'
+#
+#------------------------------------------------------------------------------
+
+require 'bundler/setup'
+require 'optparse'
+require_relative 'lib/importer'
+require_relative 'lib/pseudoapi'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: ./ponymaker.rb -f questions.txt -o exam.db"
+
+  opts.on("-fFILE", "--file=FILE", "Input question file") do |f|
+    options[:file] = f
+  end
+
+  opts.on("-oDB", "--output=DB", "Output database file") do |db|
+    options[:db] = db
+  end
+end.parse!
+
+unless options[:file] && options[:db]
+  warn "Please specify input file (-f) and data base (-o)"
+  exit 1
+end
+
+handler = Handler.new(options[:db])
+api = PseudoApi.new
+prepod = PseudoTGUser.new(167_346_988, 'Tilir')
+chat = PseudoChat.new(1)
+
+register_event = PseudoMessage.new(prepod, chat, "/register")
+handler.process_message(api, register_event)
+
+importer = QuestionImporter.new(
+  filename: options[:file],
+  handler: handler,
+  api: api,
+  prepod: prepod,
+  chat: chat
+)
+
+importer.import!
+puts "Import done: #{options[:db]}"
